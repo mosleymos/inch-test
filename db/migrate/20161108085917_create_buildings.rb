7d8a09d23881ce270b3ef620c8442759155dec44
keita mori <keitamori@gmail.com>
2016-11-08 10:06:00 +0100

    Generate model Building

---
+++ db/migrate/20161108085917_create_buildings.rb
@@ -0,0 +1,13 @@
+class CreateBuildings < ActiveRecord::Migration[5.0]
+  def change
+    create_table :buildings do |t|
+      t.integer :reference, null: false
+      t.string :address, null: true, default: "Unknow address"
+      t.string :zip_code, null: true, default: "Unknow zip_code"
+      t.string :country, null: true, default: "Unknow country"
+      t.string :manager_name, null: true, default: "Unknow manager_name" 
+
+      t.timestamps
+    end
+  end
+end
